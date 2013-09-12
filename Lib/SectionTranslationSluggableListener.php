cfc75eb0562ff509800fb83029e1b20cc324342c
Jean-François Côté <tioiscote@gmail.com>
2013-09-12 15:38:30 -0400

    Renamed SectionSluggableListener to SectionTranslationSluggableListener Override the getSelectQueryBuilder to check the parent entity to find a slug

---
+++ Lib/SectionTranslationSluggableListener.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Egzakt\SystemBundle\Lib;
+
+use Egzakt\DoctrineBehaviorsBundle\ORM\Sluggable\SluggableListener;
+use Egzakt\DoctrineBehaviorsBundle\ORM\Sluggable\SluggableListenerInterface;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class SectionTranslationSluggableListener
+ */
+class SectionTranslationSluggableListener extends SluggableListener implements SluggableListenerInterface
+{
+
+    /**
+     * Get Entity Name
+     *
+     * Returns the name of the entity having a slug field which to map the SluggableListener
+     *
+     * @return array
+     */
+    public function getEntityName()
+    {
+        return 'Egzakt\SystemBundle\Entity\SectionTranslation';
+    }
+
+    /**
+     * Get Select Query Builder
+     *
+     * Returns the Select QueryBuilder that will check for a similar slug in the table
+     * The slug will be valid when the Query returns 0 rows.
+     *
+     * @param string $slug
+     * @param mixed $entity
+     * @param EntityManager $em
+     *
+     * @return QueryBuilder
+     */
+    public function getSelectQueryBuilder($slug, $entity, EntityManager $em)
+    {
+        $translatable = $entity->getTranslatable();
+
+        $queryBuilder = $em->createQueryBuilder()
+                ->select('DISTINCT(s.slug)')
+                ->from('Egzakt\SystemBundle\Entity\SectionTranslation', 's')
+                ->innerJoin('s.translatable', 't')
+                ->where('s.slug = :slug')
+                ->andWhere('s.locale = :locale')
+                ->setParameters([
+                        'slug' => $slug,
+                        'locale' => $entity->getLocale()
+                ]);
+
+        if ($translatable->getId()) {
+            $queryBuilder->andWhere('t.id <> :id')
+                ->setParameter('id', $translatable->getId());
+        }
+
+        if ($translatable->getParent()) {
+            $queryBuilder->andWhere('t.parent = :parent')
+                ->setParameter('parent', $translatable->getParent());
+        }
+
+        return $queryBuilder;
+    }
+
+}
