04d74787e87856ff774400509ffce1cd920e84d4
tdel <t.delemis@egzakt.com>
2013-09-16 09:46:58 -0400

    removing unneeded comment

--- DependencyInjection/Compiler/RouterExtensionCompilerPass.php
+++ DependencyInjection/Compiler/RouterExtensionCompilerPass.php
@@ -14,9 +14,6 @@ class RouterExtensionCompilerPass implements CompilerPassInterface
         // automatic routing parameters
         $container->setParameter('twig.extension.routing.class', 'Egzakt\\SystemBundle\\Extensions\\RoutingExtension');
         $container->setParameter('router.options.generator_base_class', 'Egzakt\\SystemBundle\\Lib\\RouterUrlGenerator');
-        /*$container->findDefinition('twig.extension.routing')->addMethodCall('setAutoParametersHandler', array(
-            new Reference('egzakt_system.router_auto_parameters_handler')
-        ));*/
 
         // i18n loader override
         $container->setParameter('jms_i18n_routing.loader.class', 'Egzakt\\SystemBundle\\Routing\\Loader');
