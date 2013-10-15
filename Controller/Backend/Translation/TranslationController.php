0672c7171f17c77765fe0250bd38914978c41a5d
Cedric Deschenes <cedricdeschenes@gmail.com>
2013-10-15 13:25:43 -0400

    Moved the JS'ed CSS for the flash-success popup inside the .css

--- Controller/Backend/Translation/TranslationController.php
+++ Controller/Backend/Translation/TranslationController.php
@@ -2,8 +2,8 @@
 
 namespace Flexy\SystemBundle\Controller\Backend\Translation;
 
-use Flexy\SystemBundle\Translation\Php\PhpExtractor;
-use Flexy\SystemBundle\Translation\Twig\TwigExtractor;
+use Flexy\SystemBundle\Translation\Extractor\PhpExtractor;
+use Flexy\SystemBundle\Translation\Extractor\TwigExtractor;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
